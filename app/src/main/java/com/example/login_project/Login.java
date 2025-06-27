f550438aa10f67cf2cc0a61ca9f4c1e280d70646
mafi1996 <mafiulalam1998@gmail.com>
2025-06-27 23:34:47 +0600

    Login,Signup

---
+++ app/src/main/java/com/example/login_project/Login.java
@@ -0,0 +1,25 @@
+package com.example.login_project;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Login extends AppCompatActivity {
+
+    Button btnlogin,btnsignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnlogin=findViewById(R.id.btnlogin);
+        btnsignup=findViewById(R.id.btnsignup);
+
+    }
+}
---
+++ app/src/main/java/com/example/login_project/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.login_project;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sharedPreferences = getSharedPreferences("myapp",MODE_PRIVATE);
+
+        String email = sharedPreferences.getString("email","");
+        if (email.length()<=0){
+
+            startActivity(new Intent(MainActivity.this,Login.class));
+            finish();
+        }
+
+    }
+}
