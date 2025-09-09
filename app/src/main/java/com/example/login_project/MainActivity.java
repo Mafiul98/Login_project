c54d385e8fd4bacd6c843f1dc45ed30fe4da4e4f
mafi1996 <mafiulalam1998@gmail.com>
2025-09-09 22:10:22 +0600

    logout

--- app/src/main/java/com/example/login_project/MainActivity.java
+++ app/src/main/java/com/example/login_project/MainActivity.java
@@ -4,6 +4,10 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +21,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -24,11 +29,17 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences sharedPreferences;
+    ImageView imageview;
+    TextView tvdisplay;
+    Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageview=findViewById(R.id.imageview);
+        tvdisplay=findViewById(R.id.tvdisplay);
+        logout=findViewById(R.id.logout);
         sharedPreferences = getSharedPreferences("myapp",MODE_PRIVATE);
 
         try {
@@ -42,14 +53,33 @@ public class MainActivity extends AppCompatActivity {
 
             startActivity(new Intent(MainActivity.this,Login.class));
             finish();
+        }else {
+
+            ObjectRequest();
         }
+
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("email","");
+                editor.apply();
+
+                startActivity(new Intent(MainActivity.this,Login.class));
+                finish();
+            }
+        });
+
+
+
 
     }
 //========================jason object===============================================
 
     private void ObjectRequest(){
 
-        String url = "";
+        String url = "https://mafiul.shop/home.php";
         JSONObject jsonObject = new JSONObject();
 
         try {
@@ -66,6 +96,11 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     String image = response.getString("image");
                     String result = response.getString("result");
+
+                    tvdisplay.setText(result);
+                    Picasso.get().load(image).into(imageview);
+
+
                 } catch (JSONException e) {
                     throw new RuntimeException(e);
                 }
